837ac2f470f075dbb55180e0eae5a912ab721acc
irakliSamushia19 <105517113+irakliSamushia19@users.noreply.github.com>
2022-10-29 01:53:37 +0400

    Add files via upload

---
+++ davaleba_3/database/seeders/YoutubeSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\youtube;
+
+class YoutubeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        youtube::create([
+            'title' => 'Antonio Banderas - el gato con botas - Morena de mi Corazon',
+            'link' => 'https://www.youtube.com/watch?v=MrAx-ie8yQ0&list=RDMrAx-ie8yQ0&start_radio=1',
+            'description' => 'sheyvarebuli ar myavs',
+            'views' => 10083688,
+            'punlish_date' => 'Apr 23, 2012',
+            'likes' => 72000
+        ]);
+        youtube::create([
+            'title' => 'Antonio Banderas - el gato con botas - Morena de mi Corazon',
+            'link' => 'https://www.youtube.com/watch?v=MrAx-ie8yQ0&list=RDMrAx-ie8yQ0&start_radio=1',
+            'description' => 'sheyvarebuli ar myavs',
+            'views' => 10083688,
+            'punlish_date' => 'Apr 23, 2012',
+            'likes' => 72000
+        ]);
+        youtube::create([
+            'title' => 'Antonio Banderas - el gato con botas - Morena de mi Corazon',
+            'link' => 'https://www.youtube.com/watch?v=MrAx-ie8yQ0&list=RDMrAx-ie8yQ0&start_radio=1',
+            'description' => 'sheyvarebuli ar myavs',
+            'views' => 10083688,
+            'punlish_date' => 'Apr 23, 2012',
+            'likes' => 72000
+        ]);
+        youtube::create([
+            'title' => 'Antonio Banderas - el gato con botas - Morena de mi Corazon',
+            'link' => 'https://www.youtube.com/watch?v=MrAx-ie8yQ0&list=RDMrAx-ie8yQ0&start_radio=1',
+            'description' => 'sheyvarebuli ar myavs',
+            'views' => 10083688,
+            'punlish_date' => 'Apr 23, 2012',
+            'likes' => 72000
+        ]);
+        youtube::create([
+            'title' => 'Antonio Banderas - el gato con botas - Morena de mi Corazon',
+            'link' => 'https://www.youtube.com/watch?v=MrAx-ie8yQ0&list=RDMrAx-ie8yQ0&start_radio=1',
+            'description' => 'sheyvarebuli ar myavs',
+            'views' => 10083688,
+            'punlish_date' => 'Apr 23, 2012',
+            'likes' => 72000
+        ]);
+    }
+}
